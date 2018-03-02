887bc8b6c4b65bb9ecf6c8e37f604540ee9a45a3
daiyuzang <1824454842@qq.com>
2018-03-02 15:33:42 +0800

    open the camera

--- src/components/CameraPhoto/CameraPhoto.js
+++ src/components/CameraPhoto/CameraPhoto.js
@@ -1,20 +1,54 @@
 import React, { Component } from 'react';
-import { Collapse } from 'antd';
+import { Collapse, Upload, Button } from 'antd';
 import styles from '../CameraPhoto/CameraPhoto.css';
 
 class CameraPhoto extends Component {
   
   FileContent = (file) => {
-    this.props.dispatch({
-      type: 'book/callHome',
-    });
+    console.log(file)
+
+    // Only process image files. 
+    const that = this.props;
+    if (file.type.match('image.*')) {
+      
+      const reader = new FileReader();
+      let datas = [];
+      // Closure to capture the file information.
+      reader.onload = (function(theFile) {
+          return function(e) {
+          datas.push(e.target.result);
+          datas.push(escape(theFile.name));
+          that.dispatch({
+            type:'book/callHome',
+            payload: datas
+          })
+        };
+      })(file);
+
+      // Read in the image file as a data URL.
+      reader.readAsDataURL(file);
+    }
   }
 
   render() {
-    console.log(this.props);
     return (
-      <div>
-        <input onChange={this.FileContent} type="file" />
+      <div className={styles['input-button']}>
+        <Upload
+          name="file"
+          accept="image/*"
+          action="//jsonplaceholder.typicode.com/posts/"
+          showUploadList={false}
+          beforeUpload={this.FileContent}
+        >
+          <Button>Upload</Button>
+        </Upload>
+        <output
+          id="list"
+          className={styles['image']}>
+          <span>
+            <img src={this.props.book.imageSrc} alt="bar-code" title={this.props.book.imageTitle} />
+          </span>
+        </output>
       </div>
     );
   }
