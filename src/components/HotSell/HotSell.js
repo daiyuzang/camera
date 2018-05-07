df868499b8032482cc078db66b1784a398c69ae4
fanhua <1824454842@qq.com>
2018-05-07 19:12:56 +0800

    remove new title

--- src/components/HotSell/HotSell.js
+++ src/components/HotSell/HotSell.js
@@ -6,9 +6,9 @@ class HotSell extends Component {
   render() {
     const bookList = this.props.book;
     const columns = [
-        { title: 'NO', dataIndex: 'no' },
-        { title: '书名', dataIndex: 'bookName' },
-        { title: '作者', dataIndex: 'author' },
+        { title: '电商品牌', dataIndex: 'no' },
+        { title: '价格', dataIndex: 'money' },
+        { title: '书名', dataIndex: 'name' },
     ];
     const pagination = {
         pageSize: 10,
